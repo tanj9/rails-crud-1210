20d8ef2c4585c9f01de69303ca8d8798604e9860
Jerome Tan <jerome.tan@tuta.io>
2023-05-23 09:42:23 +0200

    added address to restaurant

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.create!(name: "Le Bichat", address: "11 rue Bichat 75010 Paris", rating: 4)
+Restaurant.create!(name: "Terre Rosse", address: "14 rue des Goncourt 75011 Paris", rating: 5)
